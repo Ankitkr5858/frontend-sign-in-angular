441e969dd6e9059ff6194e52fc83237de82e345c
Ankit kumar <ankitkumar@Ankits-MacBook-Pro.local>
2021-08-14 00:19:54 +0530

    backend url updated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  private URL = ' https://angular-node-auth-backend.herokuapp.com/';
+  private URL = ' https://angular-node-auth-backend.herokuapp.com/api';
   constructor(private http: HttpClient, private router: Router) { }
 
   signUpUser(user: any) {
