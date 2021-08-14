017e8f8f9b584c35e21b9463a606c7cce17ec43d
Ankit kumar <ankitkumar@Ankits-MacBook-Pro.local>
2021-08-14 13:03:52 +0530

    url updated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  private URL = 'http://localhost:4000/api';
+  private URL = 'https://b7f3682b37f1.ngrok.io/api';
   constructor(private http: HttpClient, private router: Router) { }
 
   signUpUser(user: any) {
